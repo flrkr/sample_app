d911afd8a82f38e9135f6a2a0e09b9296ca6349e
eric <apathy_demon@hotmail.com>
2010-06-08 23:26:14 -0400

    maybe this will work

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def logo
+    base_logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+      if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
+
